98482d6ad4f10547be19538810f826dd98b53089
Axel Moriceau <contact.amoriceau@gmail.com>
2022-04-12 11:04:46 +0200

    update sql

--- sql/create.sql
+++ sql/create.sql
@@ -7,15 +7,14 @@ LIBELLE varchar(200)
 
 CREATE TABLE IF NOT EXISTS departements (
 DEP varchar(3) PRIMARY KEY,
-REG char(2) references regions(REG),
+REG char(2) references regions(REG) ON DELETE CASCADE,
 NCC varchar(200),
 LIBELLE varchar(200)
 );
 
 CREATE TABLE IF NOT EXISTS communes (
 COM varchar(5) PRIMARY KEY,
-REG char(2) references regions(REG),
-DEP char(3) references departements(DEP),
+DEP char(3) references departements(DEP) ON DELETE CASCADE,
 NCC varchar(200),
 LIBELLE varchar(200)
 );
@@ -23,22 +22,24 @@ LIBELLE varchar(200)
 
 CREATE TABLE IF NOT EXISTS departements_cheflieu (
 DEP varchar(3) PRIMARY KEY,
-CHEFLIEU char(5) references communes(COM)
+CHEFLIEU char(5) references communes(COM) ON DELETE CASCADE
 );
 
 CREATE TABLE IF NOT EXISTS regions_cheflieu (
 REG varchar(2) PRIMARY KEY,
-CHEFLIEU char(5) references communes(COM)
+CHEFLIEU char(5) references communes(COM) ON DELETE CASCADE
 );
 
-CREATE TABLE IF NOT EXISTS statistiques (
-CODGEO varchar(5) PRIMARY KEY REFERENCES communes(COM),
-ANNEE smallint PRIMARY KEY,
-INDICATEUR varchar(5) PRIMARY KEY REFERENCES indicateurs(CODE),
-VALEUR numeric DEFAULT 0 NOT NULL
-);
 
 CREATE TABLE IF NOT EXISTS indicateurs (
 CODE varchar(5) PRIMARY KEY,
-LIBELLE varchar(255),
+LIBELLE varchar(255)
+);
+
+CREATE TABLE IF NOT EXISTS statistiques (
+CODGEO varchar(5)REFERENCES communes(COM) ON DELETE CASCADE,
+ANNEE smallint,
+INDICATEUR varchar(5) REFERENCES indicateurs(CODE) ON DELETE CASCADE,
+VALEUR numeric DEFAULT 0 NOT NULL,
+PRIMARY KEY (CODGEO, ANNEE, INDICATEUR)
 );
