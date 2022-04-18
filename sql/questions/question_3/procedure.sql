44929ed5668d6798b10cfe32effe74108e49feb4
Axel Moriceau <contact.amoriceau@gmail.com>
2022-04-18 19:02:30 +0200

    mise a jour procédure pour utiliser la population des départements

--- sql/questions/question_3/procedure.sql
+++ sql/questions/question_3/procedure.sql
@@ -4,19 +4,19 @@ as $$
   declare
     pop record;
 begin
-for pop in (select reg , population from vue_infos_regions vr where annee = 2018) loop
-
-                    update regions
-                    set population = pop.population
-                    where regions.reg = pop.reg;
-
-                end loop;
-
 for pop in (select dep,population from vue_infos_departements vd where annee = 2018) loop
 
                     update departements
                     set population = pop.population
                     where departements.dep = pop.dep;
+
+                end loop;
+                
+for pop in (select reg , sum(population) pop from departements d group by reg) loop
+
+                    update regions
+                    set population = pop.pop
+                    where regions.reg = pop.reg;
 
                 end loop;
             end;
